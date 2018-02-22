a648920d6d16951e7094c5dbb8d8a0d46efe2d91
Debojyoti Chakraborty <debojyoti@promactinfo.com>
2018-02-22 14:12:46 +0530

    Modified Program.cs File

--- UnitTestAssignment/UnitTestAssignment/Program.cs
+++ UnitTestAssignment/UnitTestAssignment/Program.cs
@@ -10,18 +10,7 @@ namespace UnitTestAssignment
     {
         static void Main(string[] args)
         {
-            var list = new List<Product>();
-            list.Add(new Product("Lettuce", 10.5, 50, "Leafy green"));
-            list.Add(new Product("Cabbage", 20, 100, "Cruciferous"));
-            list.Add(new Product("Pumpkin", 30, 30, "Marrow"));
-            list.Add(new Product("Cauliflower", 10, 25, "Cruciferous"));
-            list.Add(new Product("Zucchini", 20.5, 50, "Marrow"));
-            list.Add(new Product("Yam", 30, 50, "Root"));
-            list.Add(new Product("Spinach", 10, 100, "Leafy green"));
-            list.Add(new Product("Broccoli", 20.2, 75, "Cruciferous"));
-            list.Add(new Product("Garlic", 30, 20, "Leafy green"));
-            list.Add(new Product("Silverbeet", 10, 50, "Marrow"));
-            var productRepo = new ProductRepository(list);
+            
         }
     }
 }
